2bec2e9d3c6f10db01e442bcdab63eb3a2bcc4ee
drorcohen314 <drorc216@gmail.com>
2023-11-18 00:08:22 +0200

    fix webhook route

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -33,7 +33,7 @@ class MessagesController < ApplicationController
             else
                 @id = max_id + 1
             end
-            @callback_path = 'https://' + request.host.to_s + ':' + request.port.to_s + 'messages/update_status/' + @id.to_s
+            @callback_path = 'https://' + request.host.to_s + ':' + request.port.to_s + '/messages/update_status/' + @id.to_s
             message = client.messages.create(
             from: from,
             to: to,

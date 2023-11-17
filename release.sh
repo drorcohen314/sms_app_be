c1a9ac52c84495184c14d71d3a188fd9def022f8
drorcohen314 <drorc216@gmail.com>
2023-11-17 22:06:10 +0200

    remove sudo

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 # Custom commands to run during release phase
 bundle install
-sudo rails db:create RAILS_ENV=production
+rails db:create RAILS_ENV=production
 rails db:migrate RAILS_ENV=production
